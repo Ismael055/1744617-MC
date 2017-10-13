6d08bd9b95e09585ece72cfcaf7f32dce8d99227
Ismael055 <30933962+Ismael055@users.noreply.github.com>
2017-10-13 11:47:14 -0500

    Add files via upload

---
+++ fibonacci_3.py
@@ -0,0 +1,19 @@
+>>> memo={}
+>>> cnt=0
+>>> def fibonacci(n):
+	global memo,cnt
+	cnt+=1
+	if n==0 or n==1:
+		return(1)
+	if n in memo:
+		return memo[n]
+	else:
+		val=fibonacci(n-2)+fibonacci(n-1)
+		memo[n]=val
+		return val
+
+	
+>>> fibonacci(7)
+21
+>>> memo
+{2: 2, 3: 3, 4: 5, 5: 8, 6: 13, 7: 21}
